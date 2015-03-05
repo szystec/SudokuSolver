6f233fe557ba4780a8498da3f02565d7da5c603d
szystec <toffikers@gmail.com>
2015-03-05 20:50:22 +0000

    Camera - alpha 2 test

--- Sudoku Photo Solver/www/js/placeholders.js
+++ Sudoku Photo Solver/www/js/placeholders.js
@@ -1,8 +1,20 @@
-(function($) {
-  function onDeviceReady() {
+var app = {
+  // Application Constructor
+  initialize: function() {
+    this.bindEvents();
+  },
+
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+
+  onDeviceReady: function() {
     StatusBar.overlaysWebView(false);
     StatusBar.backgroundColorByName("gray");
-
     $('#camera').on('click', function() {
       navigator.camera.getPicture(onSuccess, onFail, {
         quality: 50,
@@ -11,16 +23,15 @@
         targetHeight: 300,
       });
     });
-  }
+  },
 
-  function onSuccess(imageData) {
+  onSuccess: function(imageData) {
     var image = document.getElementById('myImage');
     image.src = "data:image/jpeg;base64," + imageData;
-  }
+  },
 
-  function onFail(message) {
+  onFail: function(message) {
     alert('Failed because: ' + message);
   }
 
-  document.addEventListener("deviceready", onDeviceReady, false);
-})(jQuery);
+};
