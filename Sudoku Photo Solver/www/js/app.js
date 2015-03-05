b6b6ac122b8dc38c21ed4580a0c18a6a0efec32c
szystec <toffikers@gmail.com>
2015-03-05 14:59:08 +0000

    Camera - alpha
    Camera works, takes image and loads it to the app. Performance issue due
    to long Deviceready process. Affects iOS status bar and other Cordova
    tools. Might be solved by actual build.

--- Sudoku Photo Solver/www/js/app.js
+++ Sudoku Photo Solver/www/js/app.js
@@ -1,42 +1,59 @@
 (function($) {
-    "use strict";
+  "use strict";
 
-    var tipPercent = 15.0;
+  var tipPercent = 15.0;
 
-    var calcTip = function() {
-        var billAmt = Number( $('#billAmount').val() );
-        var tipAmt =  billAmt * tipPercent/100 ;
-        var totalAmt = billAmt + tipAmt;
-        $('#tipAmount').text('$' + tipAmt.toFixed(2));
-        $('#totalAmount').text('$' + totalAmt.toFixed(2));
-    };
+  var calcTip = function() {
+    var billAmt = Number($('#billAmount').val());
+    var tipAmt = billAmt * tipPercent / 100;
+    var totalAmt = billAmt + tipAmt;
+    $('#tipAmount').text('$' + tipAmt.toFixed(2));
+    $('#totalAmount').text('$' + totalAmt.toFixed(2));
+  };
 
-    var saveSettings = function() {
-        try {
-            var tipPct = parseFloat( $('#tipPercentage').val() );
-            localStorage.setItem('tipPercentage', tipPct);
-            tipPercent = tipPct;
-            window.history.back();
-        } catch (ex) {
-            alert('Tip percentage must be a decimal value');
-        }
-    };
+  var saveSettings = function() {
+    try {
+      var tipPct = parseFloat($('#tipPercentage').val());
+      localStorage.setItem('tipPercentage', tipPct);
+      tipPercent = tipPct;
+      window.history.back();
+    } catch (ex) {
+      alert('Tip percentage must be a decimal value');
+    }
+  };
 
-    $( document ).on( "ready", function(){
-        $('#calcTip').on('click', calcTip);
-        $('#saveSettings').on('click', saveSettings);
-        var tipPercentSetting = localStorage.getItem('tipPercentage');
-        if (tipPercentSetting) {
-            tipPercent = parseFloat(tipPercentSetting);
-        }
-        $('#tipPercentage').val(tipPercent);
+  $(document).on("ready", function() {
+    $('#calcTip').on('click', calcTip);
+    $('#saveSettings').on('click', saveSettings);
+    var tipPercentSetting = localStorage.getItem('tipPercentage');
+    if (tipPercentSetting) {
+      tipPercent = parseFloat(tipPercentSetting);
+    }
+    $('#tipPercentage').val(tipPercent);
+  });
+
+  function onDeviceReady() {
+    StatusBar.overlaysWebView(false);
+    StatusBar.backgroundColorByName("gray");
+
+    $('#camera').on('click', function() {
+      navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 300,
+        targetHeight: 300,
+      });
     });
+  }
 
-    $( document ).on( "deviceready", function(){
-        StatusBar.overlaysWebView( false );
-        StatusBar.backgroundColorByName("gray");
-    });
+  function onSuccess(imageData) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
+  }
 
-}
-)(jQuery);
+  function onFail(message) {
+    alert('Failed because: ' + message);
+  }
 
+  document.addEventListener("deviceready", onDeviceReady, false);
+})(jQuery);
