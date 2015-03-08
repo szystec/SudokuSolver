59cfff26c46ab60bfd82ec544a55a07872aa8b03
szystec <toffikers@gmail.com>
2015-03-08 20:23:21 +0000

    Working Camera action with onclick element

--- Sudoku Photo Solver/www/js/app.js
+++ Sudoku Photo Solver/www/js/app.js
@@ -1,77 +1,48 @@
 var camera;
+
 // Wait for device API libraries to load
 //
 document.addEventListener("deviceready", onDeviceReady, false);
-
-function id(element) {
-  return document.getElementById(element);
-}
 
 // device APIs are available
 //
 function onDeviceReady() {
   camera = navigator.camera;
-  cameraApp = new cameraApp();
-  cameraApp.run();
 }
 
-function cameraApp() {}
+// Called when a photo is successfully retrieved
+//
+function onPhotoDataSuccess(imageData) {
+     // Uncomment to view the image file URI
+     // console.log(imageURI);
 
-cameraApp.prototype = {
-  _pictureSource: null,
+     // Get image handle
+     //
+     var image = document.getElementById('myImage');
 
- _destinationType: null,
+     // Unhide image elements
+     //
+     image.style.display = 'block';
 
-  run: function() {
-    var that = this;
-    that._pictureSource = navigator.camera.PictureSourceType;
-    that._destinationType = navigator.camera.DestinationType;
-    id("cameraBtn").addEventListener("click", function() {
-      that._capturePhoto(that, arguments);
-    });
-  },
+     // Show the captured photo
+     // The in-line CSS rules are used to resize the image
+     //
+     image.src = "data:image/jpeg;base64," + imageData;
+   }
 
-  // Called when a photo is successfully retrieved
-  //
-  _onPhotoDataSuccess: function(imageData) {
-    // Uncomment to view the image file URI
-    // console.log(imageURI);
+// A button will call this function
+//
+function capturePhoto() {
+  // Take picture using device camera and retrieve image as image file URI
 
-    // Get image handle
-    //
-    var image = document.getElementById('myImage');
+  this.camera.getPicture(onPhotoDataSuccess, onFail, {
+    quality: 50,
+    destinationType: Camera.DestinationType.DATA_URL
+  });
+}
 
-    // Unhide image elements
-    //
-    image.style.display = 'block';
-
-    // Show the captured photo
-    // The in-line CSS rules are used to resize the image
-    //
-    image.src = "data:image/jpeg;base64," + imageData;
-  },
-
-  // A button will call this function
-  //
-  _capturePhoto: function() {
-    var that = this;
-
-    // Take picture using device camera and retrieve image as image file URI
-
-    this.camera.getPicture(function() {
-      that._onPhotoDataSuccess(that, arguments);
-    }, function(){
-      that._onFail(that, arguments);
-    }, {
-      quality: 50,
-      destinationType: Camera.DestinationType.DATA_URL
-    });
-  },
-
-  // Called if something bad happens.
-  //
-  _onFail: function(message) {
-    alert('Image not taken, because: ' + message);
-  }
-
-};
+// Called if something bad happens.
+//
+function onFail(message) {
+  alert('Image not taken, because: ' + message);
+}
