9e6b359bfd5663609b7431b99e2195af5fd14f27
szystec <toffikers@gmail.com>
2015-04-16 21:06:27 +0100

    Backtracking solver added, needs a lot of cleanup

--- Sudoku Photo Solver/www/js/app.js
+++ Sudoku Photo Solver/www/js/app.js
@@ -11,7 +11,8 @@ function onDeviceReady() {
   camera = navigator.camera;
 }
 
-function processImage(image){
+function processImage(){
+  var image = document.getElementById('camImage');
   var canvas = document.getElementById('filterCanvas');
   canvas.width = image.width;
   canvas.height = image.height;
@@ -22,6 +23,7 @@ function processImage(image){
 
   image.src = canvas.toDataURL("image/jpeg");
   $("#camImage").addClass("imageProcessed");
+
 }
 
 // Called when a photo is successfully retrieved
@@ -43,7 +45,31 @@ function onPhotoDataSuccess(imageData) {
 
 
 function ocr() {
-  var img = document.getElementById('camImage');
+/*
+  var image = document.getElementById('camImage');
+				var raw = [];
+				var letters = OCRAD(image, { verbose: true }).letters;
+				// here's a list of recognized letters and their corresponding coordinates and confidences
+				var wheel_of_fortune = /[123456789]/i;
+				var image_offset = image.getBoundingClientRect();
+				letters.filter(function(letter){
+					// select the letters which have vanna white's approval
+					return letter.matches.some(function(match){
+						return wheel_of_fortune.test(match.letter);
+					});
+				}).forEach(function(letter){
+					// draw a little yellow box over their faces
+					var highlight = document.createElement('div');
+					highlight.className = 'highlight';
+					highlight.style.top = (letter.y + image_offset.top) + 'px';
+					highlight.style.left = (letter.x + image_offset.left) + 'px';
+					highlight.style.width = letter.width + 'px';
+					highlight.style.height = letter.height + 'px';
+					document.body.appendChild(highlight);
+				});*/
+
+
+var img = document.getElementById('camImage');
   var ocrText = OCRAD(img,{numeric: true});
   var output = document.getElementById("ocr");
   output.innerHTML = ocrText;
