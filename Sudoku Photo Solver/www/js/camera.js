a307ee3400df70e66443ae9aa787b52a7538aa12
szystec <toffikers@gmail.com>
2015-04-22 02:31:18 +0100

    update

---
+++ Sudoku Photo Solver/www/js/camera.js
@@ -0,0 +1,94 @@
+var camera;
+var imageSource = 1;
+
+// Wait for device API libraries to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// device APIs are available
+//
+function onDeviceReady() {
+  camera = navigator.camera;
+}
+
+// Called if something bad happens.
+//
+function onFail(message) {
+  $(".imageFail").removeClass("hide");
+  $.mobile.loading("hide");
+}
+
+function ocr() {
+  var img = document.getElementById('camImage');
+  var ocrText = OCRAD(img, {
+    numeric: true
+  });
+
+  var row = 0;
+  var column = 0;
+  for (var i = 0; i < ocrText.length; i++) {
+    if (ocrText[i] == parseInt(ocrText[i])) {
+      var sudokuPlane = document.getElementById(row.toString() + column.toString());
+      sudokuPlane.innerHTML = ocrText[i];
+      column++;
+      if (column == 9) {
+        row++;
+        column = 0;
+      }
+    }
+  }
+
+
+  $("#camera").addClass("hide");
+  $(img).addClass("imageProcessed");
+  if (!$(".processButtons").hasClass("hide")) {
+    $(".processButtons").addClass("hide");
+  }
+  $.mobile.loading("hide");
+  //  navigator.camera.cleanup();
+}
+
+function processImage() {
+  var image = document.getElementById('camImage');
+  var canvas = document.getElementById('filterCanvas');
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  var ctx = canvas.getContext("2d");
+  var filtered = Filters.filterImage(Filters.threshold, image, 120);
+  ctx.putImageData(filtered, 0, 0);
+
+  image.src = canvas.toDataURL("image/jpeg");
+
+  this.ocr();
+}
+
+// Called when a photo is successfully retrieved
+//
+function onPhotoDataSuccess(imageData) {
+  var img = document.getElementById('camImage');
+  img.src = "data:image/jpeg;base64," + imageData;
+
+  $(".processButtons").removeClass("hide");
+  this.processImage();
+}
+
+function capturePhoto() {
+  this.imageSource = 1;
+  this.getPhoto();
+}
+
+function loadPhoto() {
+  this.imageSource = 2;
+  this.getPhoto();
+}
+
+function getPhoto() {
+  this.camera.getPicture(onPhotoDataSuccess, onFail, {
+    quality: 75,
+    destinationType: Camera.DestinationType.DATA_URL,
+    sourceType: imageSource,
+    targetWidth: 800,
+    targetHeight: 800
+  });
+}
--- Sudoku Photo Solver/www/js/viewController.js
+++ Sudoku Photo Solver/www/js/viewController.js
@@ -1,42 +1,76 @@
 //loading spinner
-$( document ).on( "click", ".show-page-loading-msg", function() {
-    var $this = $( this ),
-        theme = $this.jqmData( "theme" ) || $.mobile.loader.prototype.options.theme,
-        msgText = $this.jqmData( "msgtext" ) || $.mobile.loader.prototype.options.text,
-        textVisible = $this.jqmData( "textvisible" ) || $.mobile.loader.prototype.options.textVisible,
-        textonly = !!$this.jqmData( "textonly" );
-        html = $this.jqmData( "html" ) || "";
-    $.mobile.loading( "show", {
-            text: msgText,
-            textVisible: textVisible,
-            theme: theme,
-            textonly: textonly,
-            html: html
-    });
+$(document).on("click", ".show-page-loading-msg", function() {    
+  var $this = $(this),
+            theme = $this.jqmData("theme") || $.mobile.loader.prototype.options.theme,
+            msgText = $this.jqmData("msgtext") || $.mobile.loader.prototype.options.text,
+            textVisible = $this.jqmData("textvisible") || $.mobile.loader.prototype.options.textVisible,
+            textonly = !!$this.jqmData("textonly");        
+  html = $this.jqmData("html") || "";    
+  $.mobile.loading("show", {            
+    text: msgText,
+                textVisible: textVisible,
+                theme: theme,
+                textonly: textonly,
+                html: html    
+  });
 });
+
+function cameraImage() {
+  capturePhoto();
+
+  if (!$(".imageFail").hasClass("hide")) {
+    $(".imageFail").addClass("hide");
+  }
+
+  $(".ui-loader").hide();
+}
+
+function libraryImage() {
+  loadPhoto();
+
+  if (!$(".imageFail").hasClass("hide")) {
+    $(".imageFail").addClass("hide");
+  }
+  $(".ui-loader").hide();
+}
+
+function manipulateImage() {
+  processImage();
+  $(".ui-loader").hide();
+}
+
+function readImage() {
+  ocr();
+  $(".ui-loader").hide();
+
+}
+
+function editSudoku() {
+  $("#grid").addClass("edit");
+}
 
 function solvePuzzle() {
   var solver = new Solver();
   var row, column;
-  for ( row = 0; row < 9; row++) {
-    for ( column = 0; column < 9; column++) {
+  for (row = 1; row < 10; row++) {
+    for (column = 1; column < 10; column++) {
       var value = document.getElementById(row.toString() + column.toString()).innerText;
       if (value != '0') {
-        solver.sudokuGrid[row][column] = value;
+        solver.sudokuGrid[row - 1][column - 1] = value;
       }
     }
   }
   if (solver.initiateSolver()) {
-    for ( row = 0; row < 9; row++) {
-      for ( column = 0; column < 9; column++) {
+    for (row = 1; row < 10; row++) {
+      for (column = 1; column < 10; column++) {
         var cell = document.getElementById(row.toString() + column.toString());
-        cell.innerHTML = solver.sudokuGrid[row][column];
+        cell.innerHTML = solver.sudokuGrid[row - 1][column - 1];
       }
     }
   } else {
     alert("Unable to solve the puzzle!");
   }
-//  $.mobile.loading().hide();
+  //  $.mobile.loading().hide();
 }
 
 function isNumberKey(evt) {
@@ -46,13 +80,39 @@ function isNumberKey(evt) {
   return true;
 }
 
-function onTextFieldChanged(evt) {
+function colour() {
+  console.log("im there");
+}
+
+function interact(evt) {
   var caller = evt.target || evt.srcElement;
 
-  if (caller.innerText == 2) {
-    caller.style.backgroundColor = '#afffff';
-  } else {
-    caller.style.backgroundColor = '#e92929';
+  if ($("#grid").hasClass("edit")) {
+    var child = $(caller).children().last();
+
+    if ($("#grid").hasClass("tagged")) {
+      var element = $(".tag", "#grid");
+      var digit = element.text();
+
+      if (caller.innerText == ""){
+        element.text(child.innerText);
+        child.innerText = digit;
+      }else {
+        element.text(caller.innerText);
+        caller.innerText = digit;
+      }
+      $("#grid").removeClass("tagged");
+      $(element).removeClass("tag");
+
+    } else {
+      if (caller.innerText == "") {
+        $(child).addClass("tag");
+
+      } else {
+        $(caller).addClass("tag");
+      }
+      $("#grid").addClass("tagged");
+    }
   }
 }
 
