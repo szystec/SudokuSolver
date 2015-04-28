0938d7bc3efa0d1595c5c8e4616e2657670a1e52
szystec <toffikers@gmail.com>
2015-04-28 12:42:35 +0100

    updates

--- Sudoku Photo Solver/www/js/viewController.js
+++ Sudoku Photo Solver/www/js/viewController.js
@@ -47,6 +47,18 @@ function readImage() {
 
 function editSudoku() {
   $("#grid").addClass("edit");
+  $(".userInput").addClass("hide");
+  $(".editGrid").removeClass("hide");
+}
+
+function editDone() {
+  $("#grid").removeClass("edit");
+  $(".userInput").removeClass("hide");
+  $(".editGrid").addClass("hide");
+  if ($("#grid").hasClass("tagged")) {
+    $("#grid.tagged").find(".tag").css("background-color", "#f9f9f9").removeClass("tag");
+    $("#grid").removeClass("tagged");
+  }
 }
 
 function solvePuzzle() {
@@ -65,6 +77,7 @@ function solvePuzzle() {
       for (column = 1; column < 10; column++) {
         var cell = document.getElementById(row.toString() + column.toString());
         cell.innerHTML = solver.sudokuGrid[row - 1][column - 1];
+        cell.style.color = "green";
       }
     }
   } else {
@@ -73,48 +86,62 @@ function solvePuzzle() {
   //  $.mobile.loading().hide();
 }
 
-function isNumberKey(evt) {
-  var charCode = (evt.which) ? evt.which : event.keyCode;
-  if (charCode > 31 && (charCode < 49 || charCode > 57))
-    return false;
-  return true;
+function moveDigits(caller){
+  var child = $(caller).children("p").first();
+
+  if ($("#grid").hasClass("tagged")) {
+    var element = $(".tag", "#grid");
+    var digit = element.text();
+
+    if ($(caller).prop("tagName") == "TD") {
+      element.text("");
+      child.text(digit);
+    } else {
+      element.text(caller.innerText);
+      caller.innerText = digit;
+    }
+    $("#grid").removeClass("tagged");
+    $(element).removeClass("tag").css("background-color", "#f9f9f9");
+
+  } else {
+    if ($(caller).prop("tagName") == "TD") {
+      $(child).addClass("tag");
+      $(child).css("background-color", "#98ebe4");
+
+    } else {
+      $(caller).addClass("tag").css("background-color", "#98ebe4");
+    }
+    $("#grid").addClass("tagged");
+
+  }
 }
 
-function colour() {
-  console.log("im there");
+function playGame(caller){
+  //at the begining remove class input and tapped
+  //to clear previous selectiona and select a new one
+
+  var child = $(caller).children("p").first();
+
+  if ($("#grid").hasClass("input")){
+
+  } else {
+    if ($(caller).prop("tagName") == "TD") {
+      $(caller).addClass("tapped");
+      $(caller).css("background-color", "#93ed94");
+      //#grid set class input
+    }
+  }
 }
 
 function interact(evt) {
-  var caller = evt.target || evt.srcElement;
+  var call = evt.target || evt.srcElement;
 
   if ($("#grid").hasClass("edit")) {
-    var child = $(caller).children("p").first();
-
-    if ($("#grid").hasClass("tagged")) {
-      var element = $(".tag", "#grid");
-      var digit = element.text();
-
-      if ($(caller).prop("tagName") == "TD"){
-        element.text("");
-        child.text(digit);
-      }else {
-        element.text(caller.innerText);
-        caller.innerText = digit;
-      }
-      $("#grid").removeClass("tagged");
-      $(element).removeClass("tag");
-
-    } else {
-      if ($(caller).prop("tagName") == "TD") {
-        $(child).addClass("tag");
-
-      } else {
-        $(caller).addClass("tag");
-      }
-      $("#grid").addClass("tagged");
-      $(caller).css("color", "blue");
-    }
+    this.moveDigits(call);
+  } else {
+    this.playGame(call);
   }
+
 }
 
 function clearGrid() {
